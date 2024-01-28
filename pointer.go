b3e1e78e272feb1dc0cc37547173085e21b46106
putragabrielll <putragmahk@gmail.com>
2024-01-28 11:41:38 +0700

    fix: pointer comment

--- pointer.go
+++ pointer.go
@@ -8,7 +8,7 @@ func Pointer(name string){
 	var poin *string = &biodata
 
 	fmt.Println(biodata)
-	fmt.Println(&biodata)
-	fmt.Println(*poin)
+	fmt.Println(&biodata) // "&" untuk cek memori keberapa dia terpakai 
+	fmt.Println(*poin) // "*" untuk cek isi dari suatu variable
 	fmt.Println(&poin)
 }
